cb4aa56a0ed37c21bdaf37de07ae7f55eba0de12
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-05-06 12:31:57 +0530

    debug login error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://prepbot-k3yqyqlxs-shrishtid06-4063s-projects.vercel.app"],
+    origin: ["http://localhost:5173", "https://prepbot-alpha.vercel.app"],
     credentials: true
 }));
 
