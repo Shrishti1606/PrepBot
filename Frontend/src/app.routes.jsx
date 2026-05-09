01340f1081ff87d0850bd2fd958c1bad992a1989
Shrishti1606 <shrishtidhiman04@gmail.com>
2026-05-09 15:23:58 +0530

    update pages ui

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
-import Login from "./features/auth/pages/Login.jsx";
-import Register from "./features/auth/pages/Register.jsx";
+import Log from "./features/auth/pages/Log.jsx";
+import Reg from "./features/auth/pages/Reg.jsx";
 import Protected from "./features/auth/components/Protected.jsx";
 import Home from "./features/interview/pages/Home.jsx";
 import Interview from "./features/interview/pages/interview.jsx";
@@ -10,11 +10,11 @@ import Interview from "./features/interview/pages/interview.jsx";
 export const router = createBrowserRouter([
     {
         path: "/login",
-        element: <Login/>
+        element: <Log/>
     },
     {
         path: "/register",
-        element: <Register />
+        element: <Reg />
     },
     {
         path: "/",
